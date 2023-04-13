227cbf53e2e7e70508dae0a971fbdf5dc6dd34b4
ignaciohl <100412513+ignaciohl@users.noreply.github.com>
2023-04-13 20:20:28 +0200

    Lo mismo pero ahora bien escrito

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -86,7 +86,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      auth.userDetailsService(userDatailsService);
+      auth.userDetailsService(userDetailsService);
   }
 
   @Bean
